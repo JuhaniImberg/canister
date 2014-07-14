8cc9ee795ee722dee2600d4460dc4c4d01192b68
Juhani Imberg <juhani@imberg.fi>
2014-07-14 23:46:15 +0300

    spaces plz

--- README.md
+++ README.md
@@ -6,13 +6,13 @@ A very simple URL shortening app.
 installation & running
 ======================
 
-	Python 3.3+
-	redis
-	pip install -r requirements.txt
-	edit config.py to fit your setup
-	python3 canister.py
+    Python 3.3+
+    redis
+    pip install -r requirements.txt
+    edit config.py to fit your setup
+    python3 canister.py
 
 todo
 ====
 
-	support TTL
+    support TTL
--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 config = {
-	"server-host": "0.0.0.0",
-	"server-port": 8080,
-	"server": "wsgiref",
-	"base-url": "http://localhost:8080/",
-	"redis-host": "localhost",
-	"redis-port": 6379,
-	"hashids-salt": "E1pVf785RbBNftdy8xwdtTWcvlAu7R3U",
-	"regex-name": "([a-zA-Z_\-+]){1,32}",
-	"regex-url": "^(https?):\/\/[-a-zA-Z0-9+&@#\/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#\/%=~_|]"
+    "server-host": "0.0.0.0",
+    "server-port": 8080,
+    "server": "wsgiref",
+    "base-url": "http://localhost:8080/",
+    "redis-host": "localhost",
+    "redis-port": 6379,
+    "hashids-salt": "E1pVf785RbBNftdy8xwdtTWcvlAu7R3U",
+    "regex-name": "([a-zA-Z_\-+]){1,32}",
+    "regex-url": "^(https?):\/\/[-a-zA-Z0-9+&@#\/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#\/%=~_|]"
 }
